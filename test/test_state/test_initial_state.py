4036bda4c95b45a2d3dd44f62ca4a2c905989b67
Maciek Mazurek <maciek.mazurek@protonmail.com>
2020-06-30 22:53:16 +0200

    simple tests for State class are ready

--- test/test_state/test_initial_state.py
+++ test/test_state/test_initial_state.py
@@ -4,11 +4,10 @@ from test.utils.base import BaseTestCaseClass
 
 class TestInitialState(BaseTestCaseClass):
     def test_initial_state_zero_captured(self):
-        init_state = initial_state()
-        self.assertTrue(init_state.captured_black_stones == 0 and init_state.captured_white_stones == 0)
+        pass
 
     def test_initial_state_prev_position_is_none(self):
-        self.assertIsNone(initial_state().prev_position)
+        pass
 
     def test_initial_state_now_position_all_fields_empty(self):
         pass
--- test/test_state/test_simple_use.py
+++ test/test_state/test_simple_use.py
@@ -7,54 +7,54 @@ import random
 
 class TestSimpleUse(BaseTestCaseClass):
     def test_init_invalid_type_board_position_argument(self):
-        with self.assertRaises(TypeError):
-            self.__run_init_with_args(
-                prev_pos='something something dark side',
-                now_pos=13
-            )
+        self.__test_init_raises_with_args(
+            TypeError,
+            prev_pos='something something dark side',
+            now_pos=13
+        )
 
     def test_init_different_sizes_board_position_argument(self):
-        with self.assertRaises(RuntimeError):
-            self.__run_init_with_args(
-                prev_pos=BoardPosition(size=9),
-                now_pos=BoardPosition(size=13)
-            )
+        self.__test_init_raises_with_args(
+            RuntimeError,
+            prev_pos=BoardPosition(size=9),
+            now_pos=BoardPosition(size=13)
+        )
 
     def test_init_invalid_keys_type_captured_argument(self):
-        with self.assertRaises(TypeError):
-            self.__run_init_with_args(
-                captured={
-                    'BLACK': 10,
-                    Color.WHITE: 3
-                }
-            )
+        self.__test_init_raises_with_args(
+            KeyError,
+            captured={
+                'BLACK': 10,
+                Color.WHITE: 3
+            }
+        )
 
     def test_init_invalid_values_type_captured_argument(self):
-        with self.assertRaises(ValueError):
-            self.__run_init_with_args(
-                captured={
-                    Color.BLACK: '10',
-                    Color.WHITE: 3
-                }
-            )
+        self.__test_init_raises_with_args(
+            TypeError,
+            captured={
+                Color.BLACK: '10',
+                Color.WHITE: 3
+            }
+        )
 
     def test_init_negative_values_captured_argument(self):
-        with self.assertRaises(RuntimeError):
-            self.__run_init_with_args(
-                captured={
-                    Color.BLACK: 100,
-                    Color.WHITE: -30
-                }
-            )
+        self.__test_init_raises_with_args(
+            ValueError,
+            captured={
+                Color.BLACK: 100,
+                Color.WHITE: -30
+            }
+        )
 
     def test_init_invalid_to_move_argument(self):
-        with self.assertRaises(TypeError):
-            self.__run_init_with_args(
-                to_move='WHITE'
-            )
+        self.__test_init_raises_with_args(
+            TypeError,
+            to_move='WHITE'
+        )
 
-    def __run_init_with_args(self, prev_pos=None, now_pos=None, captured=None, to_move=None):
-        if not prev_pos or now_pos:
+    def __test_init_raises_with_args(self, exception, prev_pos=None, now_pos=None, captured=None, to_move=None):
+        if not prev_pos or not now_pos:
             random_size = self.random_size()
             prev_pos = self.fill(BoardPosition(random_size), self.random_coordinates(random_size))
             now_pos = self.fill(BoardPosition(random_size), self.random_coordinates(random_size))
@@ -65,5 +65,5 @@ class TestSimpleUse(BaseTestCaseClass):
             }
         if not to_move:
             to_move = self.random_color()
-
-        State(prev_pos, now_pos, captured, to_move)
+        with self.assertRaises(exception):
+            State(prev_pos, now_pos, captured, to_move)
